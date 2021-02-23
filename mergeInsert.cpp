dae92f3a89cdbe483a92ed13205417dfdfc06f30
Ryan Dickman <rdickman1@gmail.com>
2021-02-23 17:04:22 -0600

    began driver program

---
+++ mergeInsert.cpp
@@ -0,0 +1,26 @@
+// This program is designed to preform a merge/insertion sort recursivly,
+// 	read from a file named data1.txt, and wirte to a file named out1.txt
+
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+	
+	fstream file;
+
+	file.open("data1.txt");
+	if(file.is_open()){
+
+	}
+	else {
+		cout << "File failed to open.\n";
+		cin.get();
+	}
+
+	return 0;
+
+
+
+}
